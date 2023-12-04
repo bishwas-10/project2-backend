f0e22634ae7346e927b5c2fbcf29ae3ca59392bf
suman sharma <sumans@sumans-MacBook-Air.local>
2023-12-04 21:16:55 +0545

    added backend updates

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,37 @@
-import express, { Request, Response, Application } from "express";
-require("dotenv").config();
-import cors from "cors";
-import mongoose from "mongoose";
-import router from "./routes/posts";
+import dotenv from 'dotenv';
+import express, {Application} from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import PostRouter from './routes/posts';
+import helmate from 'helmet';
+import {errorHandler} from './controllers/errorHandler.controller';
+import morgan from 'morgan';
+
+dotenv.config();
 
 const app: Application = express();
-const MONGO_URL:string = process.env.MONGO_URL ;
+const MONGO_URL: string = process.env.MONGO_URL;
 const PORT = process.env.PORT || 8000;
+
+app.use(helmate());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+app.use(morgan('dev'));
 app.use(express.json());
-
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-
-
+app.use(express.urlencoded({limit: '30mb', extended: true}));
 
 mongoose
-  .connect(MONGO_URL,{})
-  .then(() => console.log("connected to mongo db"))
+  .connect(MONGO_URL)
+  .then(() => console.log('connected to mongo db'))
   .catch((err) => console.log(err));
 
-app.use('/api/posts', router)
-app.use(cors());
+app.use('/api/post', PostRouter);
+app.use(errorHandler);
+
 app.listen(PORT, (): void => {
-  console.log(`server is listening to port ${PORT}`);
+  console.log(`server is listening on port http://localhost:${PORT}`);
 });
